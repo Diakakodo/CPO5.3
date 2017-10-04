8872281acee825a06e025a52ed110209d5f258ba
KETAM Djillali <ketam.djalal@gmail.com>
2017-10-04 15:54:13 +0200

    exo5.3 complet

--- Repertoir.java
+++ Repertoir.java
@@ -16,7 +16,7 @@ public class Repertoir extends Composant
     }
     public boolean ajouter(Composant composant)
     {
-        if(this.contient(composant)) {
+        if(composants.contains(composant)) {
             return false;
         }
         else if((composant instanceof Repertoir) && (((Repertoir) composant).contient(this))) {
@@ -26,20 +26,39 @@ public class Repertoir extends Composant
             return composants.add(composant);
         }
     }
-    
+    //calcul de taille appel a la fonction de chaque type 
+      public int calculTaille()
+    {
+        Iterator<Composant> str = composants.iterator();
+        int calcul= 0;
+        while(str.hasNext()) {
+            calcul +=str.next().calculTaille(); 
+        }
+        return calcul;
+    }
+    //retirer un coposant 
     public boolean retirer(Composant composant)
     {
         return composants.remove(composant);
     }
-    
-    public int calculTaille()
-    {
+       public boolean contient(Composant comp) {
         Iterator<Composant> str = composants.iterator();
-        int somme = 0;
-        
+        Composant tmp;
+       
         while(str.hasNext()) {
-            somme +=str.next().calculTaille(); 
+            tmp = str.next();
+            
+            if(tmp.equals(comp)){
+                return true;
+            }
+            else if(tmp instanceof Repertoir) {
+               //repertoir imbriquer
+                 if(((Repertoir) tmp).contient(comp)) {
+                    return true;
+                }
+            }
         }
-        return somme;
+        return false;
     }
+  
 }
